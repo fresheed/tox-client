d4c04d767718fc1f2bd3ac201cc01364f038666e
fresheed <ok.john.rus@gmail.com>
2017-03-21 14:04:21 +0300

    project created

---
+++ src/main/java/org/fresheed/university/ControlConsole.java
@@ -0,0 +1,18 @@
+package org.fresheed.university;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by fresheed on 18.02.17.
+ */
+public class ControlConsole {
+    public static void main(String[] args) {
+        String hostname = "localhost";
+        int port = 4141;
+        System.out.println("run");
+        List<String> strs=new ArrayList<>();
+    }
+}
